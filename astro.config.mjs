5c33e952bac7d22ebfa8170c1067103b87e454fc
Lukas Krabbe <lukas.krabbe@toolport.de>
2025-06-13 13:32:58 +0200

    Chapter 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrotest9123.netlify.app"
+  site: "https://astrotest9123.netlify.app",
+  integrations: [preact()]
 });
